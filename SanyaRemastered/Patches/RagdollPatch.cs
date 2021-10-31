ded92d10fffd90beba81c9d5d0efdbc3b07f8457
louis1706 <louismonneyron5@yahoo.com>
2021-10-31 12:03:03 +0100

    Ragdoll Size ^^

--- SanyaRemastered/Patches/RagdollPatch.cs
+++ SanyaRemastered/Patches/RagdollPatch.cs
@@ -7,7 +7,7 @@ namespace SanyaRemastered.Patches
     [HarmonyPatch(typeof(RagdollManager), nameof(RagdollManager.SpawnRagdoll))]
     public static class PreventRagdollPatch
     {
-        public static bool Prefix(RagdollManager __instance, Vector3 pos, Quaternion rot, Vector3 velocity, int classId, PlayerStats.HitInfo ragdollInfo, ref bool allowRecall, string ownerID, ref string ownerNick, int playerId)
+        public static bool Prefix(RagdollManager __instance, Vector3 pos, Quaternion rot, Vector3 velocity, int classId, PlayerStats.HitInfo ragdollInfo, bool allowRecall, string ownerID, string ownerNick, int playerId, bool _096Death = false)
         {
             if (SanyaRemastered.Instance.Config.Scp106RemoveRagdoll && ragdollInfo.Tool == DamageTypes.Scp106
                 || SanyaRemastered.Instance.Config.Scp096RemoveRagdoll && ragdollInfo.Tool == DamageTypes.Scp096) return false;
@@ -15,7 +15,22 @@ namespace SanyaRemastered.Patches
             if (SanyaRemastered.Instance.Config.Scp049Real && ragdollInfo.Tool != DamageTypes.Scp049) allowRecall = false;
 
             if (SanyaRemastered.Instance.Config.TeslaDestroyName && ragdollInfo.Tool == DamageTypes.Tesla) ownerNick = "inconue";
-            return true;
+
+            Role role = __instance.hub.characterClassManager.Classes.SafeGet(classId);
+            if (role.model_ragdoll == null)
+            {
+                return false;
+            }
+            GameObject gameObject = Object.Instantiate(role.model_ragdoll, pos + role.ragdoll_offset.position, Quaternion.Euler(rot.eulerAngles + role.ragdoll_offset.rotation));
+            
+            gameObject.transform.localScale = Player.Get(playerId).Scale;
+            Mirror.NetworkServer.Spawn(gameObject);
+            Ragdoll component = gameObject.GetComponent<Ragdoll>();
+            component.Networkowner = new global::Ragdoll.Info(ownerID, ownerNick, ragdollInfo, role, playerId);
+            component.NetworkallowRecall = allowRecall;
+            component.NetworkPlayerVelo = velocity;
+            component.NetworkSCP096Death = _096Death;
+            return false;
         }
     }
 }
