192dd29d3df882bf05ee26b6862c352798a5c5cc
louis1706 <louismonneyron5@yahoo.com>
2022-04-05 00:33:22 +0200

    StringBuilder for IntercomText

--- SanyaRemastered/Patches/IntercomPatches.cs
+++ SanyaRemastered/Patches/IntercomPatches.cs
@@ -6,6 +6,7 @@ using Respawning;
 using Exiled.API.Features;
 using System.Linq;
 using Exiled.API.Enums;
+using System.Text;
 
 namespace SanyaRemastered.Patches
 {
@@ -26,155 +27,143 @@ namespace SanyaRemastered.Patches
 	{
 		public static void Prefix(Intercom __instance)
 		{
-			try
-			{
-				if (!string.IsNullOrEmpty(SanyaRemastered.Instance.SpecialTextIntercom))
-				{
-					__instance.CustomContent = SanyaRemastered.Instance.SpecialTextIntercom;
-					return;
-				}
-				if (!SanyaRemastered.Instance.Config.IntercomInformation)
-					return;
-				if (Room.Get(RoomType.EzIntercom)?.LightsOn ?? true && SanyaRemastered.Instance.Config.IntercomBrokenOnBlackout)
-				{
-					int leftdecont = (int)Math.Truncate(DecontaminationController.Singleton.DecontaminationPhases[DecontaminationController.Singleton.DecontaminationPhases.Length - 1].TimeTrigger - Math.Truncate(DecontaminationController.GetServerTime));
-					int respawntime = (int)Math.Truncate(RespawnManager.CurrentSequence() == RespawnManager.RespawnSequencePhase.RespawnCooldown ? RespawnManager.Singleton._timeForNextSequence - RespawnManager.Singleton._stopwatch.Elapsed.TotalSeconds : 0);
-					int TimeWarhead = (int)Math.Truncate(AlphaWarheadOutsitePanel._host?.timeToDetonation ?? 0);
+            try
+            {
+                if (!string.IsNullOrEmpty(SanyaRemastered.Instance.SpecialTextIntercom))
+                {
+                    __instance.CustomContent = SanyaRemastered.Instance.SpecialTextIntercom;
+                    return;
+                }
+                if (!SanyaRemastered.Instance.Config.IntercomInformation)
+                    return;
+                if (!(Room.Get(RoomType.EzIntercom)?.LightsOn ?? true && SanyaRemastered.Instance.Config.IntercomBrokenOnBlackout))
+                {
+                    __instance.CustomContent = " ";
+                    return;
+                }
 
-					leftdecont = Mathf.Clamp(leftdecont, 0, leftdecont);
+                int leftdecont = (int)Math.Truncate(DecontaminationController.Singleton.DecontaminationPhases[DecontaminationController.Singleton.DecontaminationPhases.Length - 1].TimeTrigger - Math.Truncate(DecontaminationController.GetServerTime));
+                int respawntime = (int)Math.Truncate(RespawnManager.CurrentSequence() == RespawnManager.RespawnSequencePhase.RespawnCooldown ? RespawnManager.Singleton._timeForNextSequence - RespawnManager.Singleton._stopwatch.Elapsed.TotalSeconds : 0);
+                int TimeWarhead = (int)Math.Truncate(Warhead.DetonationTimer);
 
-					float totalvoltagefloat = 0f;
-					foreach (var i in Generator.Get(GeneratorState.Activating | GeneratorState.Engaged))
-					{
-						totalvoltagefloat += i.CurrentTime;
-					}
+                leftdecont = Mathf.Clamp(leftdecont, 0, leftdecont);
 
-					totalvoltagefloat = (int)totalvoltagefloat;
+                float totalvoltagefloat = 0f;
+                foreach (var i in Generator.Get(GeneratorState.Activating | GeneratorState.Engaged))
+                {
+                    totalvoltagefloat += i.CurrentTime;
+                }
 
-					string contentfix = string.Concat(
-								$"<color=#fffffff>──── Centre d'information FIM Epsilon-11 ────</color>\n",
-								$"Durée de la brèche : {RoundSummary.roundTime / 60:00}:{RoundSummary.roundTime % 60:00}\n",
-								$"SCP restants : {RoundSummary.singleton.CountTeam(Team.SCP) - RoundSummary.singleton.CountRole(RoleType.Scp0492):00}/{RoundSummary.singleton.classlistStart.scps_except_zombies:00}\n",
-								$"Classe-D restants : {RoundSummary.singleton.CountTeam(Team.CDP):00}/{RoundSummary.singleton.classlistStart.class_ds:00}\n",
-								$"Scientifique restants : {RoundSummary.singleton.CountTeam(Team.RSC):00}/{RoundSummary.singleton.classlistStart.scientists:00}\n",
-								$"Nine-Tailed Fox restants : {RoundSummary.singleton.CountTeam(Team.MTF):00}\n"
-								);
-					//SCP-106 Femur
-					if (Room.Get(RoomType.Hcz106)?.LightsOn ?? true)
-						if (PlayerManager.localPlayer.GetComponent<CharacterClassManager>()._lureSpj.NetworkallowContain)
-						{
-							if (OneOhSixContainer.used)
-							{
-								contentfix += $"Statut du briseur de fémur : <color=#228B22>Utilisé</color>\n";
-							}
-							else
-							{
-								contentfix += $"<color=#ff0000>Statut du briseur de fémur : Prêt</color>\n";
-							}
-						}
-						else
-						{
-							contentfix += $"Statut du briseur de fémur : Vide\n";
-						}
-					else
-					{
-						contentfix += $"Statut du briseur de fémur : <color=#228B22>No Data</color>\n";
-					}
+                totalvoltagefloat = Mathf.CeilToInt(totalvoltagefloat);
 
-					//warhead
+                StringBuilder stringBuilder = new($"<color=#fffffff>──── Centre d'information FIM Epsilon-11 ────</color>\n" +
+                            $"Durée de la brèche : {RoundSummary.roundTime / 60:00}:{RoundSummary.roundTime % 60:00}\n" +
+                            $"SCP restants : {RoundSummary.singleton.CountTeam(Team.SCP) - RoundSummary.singleton.CountRole(RoleType.Scp0492):00}/{RoundSummary.singleton.classlistStart.scps_except_zombies:00}\n" +
+                            $"Classe-D restants : {RoundSummary.singleton.CountTeam(Team.CDP):00}/{RoundSummary.singleton.classlistStart.class_ds:00}\n" +
+                            $"Scientifique restants : {RoundSummary.singleton.CountTeam(Team.RSC):00}/{RoundSummary.singleton.classlistStart.scientists:00}\n" +
+                            $"Nine-Tailed Fox restants : {RoundSummary.singleton.CountTeam(Team.MTF):00}\n"
+                            );
+                //SCP-106 Femur
+                if (Room.Get(RoomType.Hcz106)?.LightsOn ?? true)
+                    if (Scp106Container.AllowContain)
+                    {
+                        if (Scp106Container.Scp106FemurUsed)
+                        {
+                            stringBuilder.Append($"Statut du briseur de fémur : <color=#228B22>Utilisé</color>\n");
+                        }
+                        else
+                        {
+                            stringBuilder.Append($"<color=#ff0000>Statut du briseur de fémur : Prêt</color>\n");
+                        }
+                    }
+                    else
+                    {
+                        stringBuilder.Append($"Statut du briseur de fémur : Vide\n");
+                    }
+                else
+                {
+                    stringBuilder.Append($"Statut du briseur de fémur : <color=#228B22>No Data</color>\n");
+                }
 
-					if (TimeWarhead <= 10)
-					{
-						contentfix += $"<color=#ff0000>Détonation inévitable : {TimeWarhead / 60}:{TimeWarhead % 60:00}</color>\n";
-					}
-					else if (AlphaWarheadOutsitePanel._host.inProgress)
-					{
-						contentfix += $"<color=#ff0000>Explosion de l'Alpha Warhead : {TimeWarhead / 60}:{TimeWarhead % 60:00}</color>\n";
-					}
-					else
-					{
-						if (!AlphaWarheadOutsitePanel.nukeside.Networkenabled)
-						{
-							contentfix += $"Statut de l'Alpha Warhead : DÉSACTIVÉE\n";
-						}
-						else
-						{
-							contentfix += $"Statut de l'Alpha Warhead : PRÊTE\n";
-						}
-					}
+                //warhead
+                if (Warhead.IsInProgress)
+                {
+                    stringBuilder.Append($"<color=#ff0000>{(TimeWarhead > 10 ? "Explosion de l'Alpha Warhead" : "Détonation inévitable")} : {TimeWarhead / 60}:{TimeWarhead % 60:00}</color>\n");
+                }
+                else
+                {
+                    stringBuilder.Append($"Statut de l'Alpha Warhead : {(AlphaWarheadOutsitePanel.nukeside.Networkenabled ? "PRÊTE" : "DÉSACTIVÉE")}\n");
+                }
 
-					//Générateur 079
-					if (Generator.Get(GeneratorState.Engaged).Count() == 3)
-					{
-						contentfix += "Tous les générateurs du site sont activés\n";
-					}
-					else
-					{
-						contentfix += $"Puissance des générateurs : {totalvoltagefloat:0000}KVA\n";
-					}
+                //Générateur 079
+                if (Generator.Get(GeneratorState.Engaged).Count() == 3)
+                {
+                    stringBuilder.Append("Tous les générateurs du site sont activés\n");
+                }
+                else
+                {
+                    stringBuilder.Append($"Puissance des générateurs : {totalvoltagefloat:0000}KVA\n");
+                }
 
-					//décontamination
-					if (!DecontaminationController.Singleton._decontaminationBegun)
-					{
-						if (leftdecont > 30)
-						{
-							contentfix += $"Temps restant avant la décontamination de la LCZ :  {leftdecont / 60:00}:{leftdecont % 60:00}\n";
-						}
-						else if (leftdecont <= 30)
-						{
-							contentfix += $"<color=#ff0000>Temps restant avant la décontamination de la LCZ : {leftdecont / 60:00}:{leftdecont % 60:00}</color>\n";
-						}
-					}
-					else if (DecontaminationController.Singleton._decontaminationBegun)
-					{
-						contentfix += $"La décontamination de la LCZ a été effectué\n";
-					}
+                //décontamination
+                if (!DecontaminationController.Singleton._decontaminationBegun)
+                {
+                    if (leftdecont > 30)
+                    {
+                        stringBuilder.Append($"Temps restant avant la décontamination de la LCZ :  {leftdecont / 60:00}:{leftdecont % 60:00}\n");
+                    }
+                    else
+                    {
+                        stringBuilder.Append($"<color=#ff0000>Temps restant avant la décontamination de la LCZ : {leftdecont / 60:00}:{leftdecont % 60:00}</color>\n");
+                    }
+                }
+                else if (DecontaminationController.Singleton._decontaminationBegun)
+                {
+                    stringBuilder.Append($"La décontamination de la LCZ a été effectué\n");
+                }
 
-					//Prochain spawn + durée MTF
+                //Prochain spawn + durée MTF
 
-					if (RespawnManager.Singleton.NextKnownTeam == SpawnableTeamType.ChaosInsurgency)
-						contentfix += $"Les renforts se préparent\n";
-					else if (RespawnTickets.Singleton.GetAvailableTickets(SpawnableTeamType.NineTailedFox) <= 0)
-						contentfix += $"Aucun renforts prévus pour le site\n";
-					else
-						contentfix += $"Prochains renforts : {respawntime / 60:00}:{respawntime % 60:00}\n";
+                if (RespawnManager.Singleton.NextKnownTeam == SpawnableTeamType.ChaosInsurgency)
+                    stringBuilder.Append($"Les renforts se préparent\n");
+                else if (RespawnTickets.Singleton.GetAvailableTickets(SpawnableTeamType.NineTailedFox) <= 0)
+                    stringBuilder.Append($"Aucun renforts prévus pour le site\n");
+                else
+                    stringBuilder.Append($"Prochains renforts : {respawntime / 60:00}:{respawntime % 60:00}\n");
 
-					//Speak Intercom
-					if (__instance.Muted)
-					{
-						contentfix += "<color=#ff0000>Accréditation insuffisante.</color>";
-					}
-					else if (Intercom.AdminSpeaking)
-					{
-						contentfix += "<color=#ff0000>L'administrateur a la priorité sur les équipements de diffusion.</color>";
-					}
-					else if (__instance.remainingCooldown > 0f)
-					{
-						contentfix += $"Temps avant redémarrage : {Mathf.CeilToInt(__instance.remainingCooldown)} seconde{(__instance.remainingCooldown <= 1 ? "" : "s")} ";
-					}
-					else if (__instance.speechRemainingTime == -77f)
-					{
-						contentfix += $"{ReferenceHub.GetHub(__instance.Networkspeaker).nicknameSync._myNickSync} à une diffusion prioritaire";
-					}
-
-					else if (__instance.speaker is not null)
-					{
-						contentfix += $"{ReferenceHub.GetHub(__instance.Networkspeaker).nicknameSync._myNickSync} Diffuse : " + Mathf.CeilToInt(__instance.speechRemainingTime) + " secondes ";
-					}
-					else
-					{
-						contentfix += "Intercom prêt à l'emploi.";
-					}
-					__instance.CustomContent = contentfix;
-
-					return;
-				}
-				__instance.CustomContent = " ";
-			}
-			catch (Exception ex)
-			{
-				Log.Error($"Intercom::UpdateText Prefix : {ex}\n {ex.StackTrace}");
-			}
+                //Speak Intercom
+                if (__instance.Muted)
+                {
+                    stringBuilder.Append("<color=#ff0000>Accréditation insuffisante.</color>");
+                }
+                else if (Intercom.AdminSpeaking)
+                {
+                    stringBuilder.Append("<color=#ff0000>L'administrateur a la priorité sur les équipements de diffusion.</color>");
+                }
+                else if (__instance.remainingCooldown > 0f)
+                {
+                    stringBuilder.Append($"Temps avant redémarrage : {Mathf.CeilToInt(__instance.remainingCooldown)} seconde{(__instance.remainingCooldown <= 1 ? "" : "s")} ");
+                }
+                else if (__instance.speechRemainingTime == -77f)
+                {
+                    stringBuilder.Append($"{Player.Get(__instance.Networkspeaker)?.DisplayNickname} à une diffusion prioritaire");
+                }
+                else if (__instance.speaker is not null)
+                {
+                    stringBuilder.Append($"{Player.Get(__instance.Networkspeaker)?.DisplayNickname} Diffuse : {Mathf.CeilToInt(__instance.speechRemainingTime)} secondes ");
+                }
+                else
+                {
+                    stringBuilder.Append("Intercom prêt à l'emploi.");
+                }
+                __instance.CustomContent = stringBuilder.ToString();
+                stringBuilder.Clear();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Intercom::UpdateText Prefix : {ex}\n {ex.StackTrace}");
+            }
 		}
 	}
 }
